b03d0dca35420bd08e4724e2e6d216de443e7135
Ricardo Rocha Ribeiro <r3ifes@gmail.com>
2025-07-06 14:47:00 -0300

    refactor: reuse sidebar layout

--- src/main.tsx
+++ src/main.tsx
@@ -12,8 +12,8 @@ import FlowPlayer from "./pages/FlowPlayer";
 import Analytics from "./pages/Analytics";
 import FlowEditor from "./pages/FlowEditor";
 import Settings from "./pages/Settings";
-import AuditTrail from "./pages/AuditTrail";
-import FlowImportExport from "./pages/FlowImportExport";
+import Audit from "./pages/Audit";
+import ImportExport from "./pages/ImportExport";
 import PathAnalytics from "./pages/PathAnalytics";
 import CustomComponents from "./pages/CustomComponents";
 
@@ -39,10 +39,10 @@ createRoot(document.getElementById("root")!).render(
         <Route path="/settings" element={<Settings />} />
 
         {/* Log de auditoria */}
-        <Route path="/audit" element={<AuditTrail />} />
+        <Route path="/audit" element={<Audit />} />
 
         {/* Importação e exportação de fluxos */}
-        <Route path="/import-export" element={<FlowImportExport />} />
+        <Route path="/import-export" element={<ImportExport />} />
 
         {/* Path Analytics */}
         <Route path="/path-analytics" element={<PathAnalytics />} />
---
+++ src/pages/ImportExport.tsx
@@ -0,0 +1,10 @@
+import SidebarLayout from "../components/Sidebar";
+import FlowImportExport from "./FlowImportExport";
+
+export default function ImportExport() {
+  return (
+    <SidebarLayout title="Import/Export">
+      <FlowImportExport />
+    </SidebarLayout>
+  );
+}
